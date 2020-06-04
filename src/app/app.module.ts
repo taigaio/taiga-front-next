9c22c6042cf06c82d7f301a4cd63e84f92d32a54
Juanfran <juanfran.ag@gmail.com>
2020-06-04 10:10:38 +0200

    StoreDevtoolsModule only in dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { extModules } from './build-specifics';
 
 @NgModule({
   declarations: [
@@ -25,7 +24,7 @@ import { StoreRouterConnectingModule } from '@ngrx/router-store';
         strictActionTypeUniqueness: true,
       },
     }),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    extModules,
     EffectsModule.forRoot([]),
     StoreRouterConnectingModule.forRoot()
   ],
