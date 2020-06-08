79f91fc51bfa9b111f2422d05adc341fb8b92a07
Xaviju <xaviju@gmail.com>
2020-06-08 15:21:19 +0200

    build(linter): add stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  extends: ['stylelint-config-standard'],
+  plugins: ['stylelint-order'],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['define-mixin', 'mixin'],
+      },
+    ],
+    'color-named': 'never',
+    'declaration-no-important': true,
+    'font-family-name-quotes': 'always-unless-keyword',
+    'function-url-quotes': 'always',
+    'max-nesting-depth': 3,
+    'number-leading-zero': 'never',
+    'order/properties-alphabetical-order': true,
+    'selector-attribute-quotes': 'always',
+    'selector-max-compound-selectors': 3,
+    'selector-type-no-unknown': [
+      true,
+      {
+        ignore: ['custom-elements'],
+      },
+    ],
+    'string-quotes': 'double',
+  },
+};
