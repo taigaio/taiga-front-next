4c0e111ba18bc46d9faab98efe3df234d0d75d0c
Juanfran <juanfran.ag@gmail.com>
2020-06-08 11:45:10 +0200

    feat(core): add license headers

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2014-2020 Taiga Agile LLC
+ *
+ * This source code is licensed under the terms of the
+ * GNU Affero General Public License found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
