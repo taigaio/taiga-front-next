92617a4fefaf87f00a50846cb3542aad1a759821
Juanfran <juanfran.ag@gmail.com>
2020-06-09 13:58:11 +0200

    feat(core): license headers

--- src/app/api/stats/stats.module.ts
+++ src/app/api/stats/stats.module.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2014-2020 Taiga Agile LLC
+ *
+ * This source code is licensed under the terms of the
+ * GNU Affero General Public License found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
