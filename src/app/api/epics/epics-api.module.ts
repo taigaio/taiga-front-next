e72ce5e029e41dd537ef69706d7b57c2a383b7f6
Xaviju <xaviju@gmail.com>
2020-06-29 15:58:47 +0200

    feat(api): api http module

--- src/app/api/epics/epics-api.module.ts
+++ src/app/api/epics/epics-api.module.ts
@@ -7,6 +7,7 @@
  */
 
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ApiRestInterceptorModule } from '@/app/commons/api-rest-interceptor/api-rest-interceptor.module';
 
@@ -17,6 +18,7 @@ import { EpicsCustomAttributesValuesApiService } from './epics-custom-attributes
 @NgModule({
   declarations: [],
   imports: [
+    HttpClientModule,
     ApiRestInterceptorModule,
   ],
   providers: [
