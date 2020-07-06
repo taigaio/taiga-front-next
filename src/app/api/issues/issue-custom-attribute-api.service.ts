4a798de99c34bb1d84c8ebecd29df3a141229d02
Juanfran <juanfran.ag@gmail.com>
2020-07-06 14:59:36 +0200

    feat(api): issues custom attributes

---
+++ src/app/api/issues/issue-custom-attribute-api.service.ts
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2014-2020 Taiga Agile LLC
+ *
+ * This source code is licensed under the terms of the
+ * GNU Affero General Public License found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import { Injectable } from '@angular/core';
+import { ConfigService } from '@/app/config.service';
+import { HttpClient } from '@angular/common/http';
+import { IssueCustomAttribute, IssueCustomAttributeCreationData } from './issue-custom-attribute.model';
+
+@Injectable()
+export class IssueCustomAttributeApiService {
+  constructor(private http: HttpClient, private config: ConfigService) { }
+
+  public get base() {
+    return `${this.config.apiUrl}/issue-custom-attributes`;
+  }
+
+  public list(projectId: number) {
+    return this.http.get<IssueCustomAttribute[]>(this.base, {
+      params: {
+        project: projectId.toString(),
+      },
+    });
+  }
+
+  public create(issueCustomAttribute: IssueCustomAttributeCreationData) {
+    return this.http.post<IssueCustomAttribute>(this.base, issueCustomAttribute);
+  }
+
+  public get(id: number) {
+    return this.http.get<IssueCustomAttribute>(`${this.base}/${id}`);
+  }
+
+  public put(id: number, us: Partial<IssueCustomAttribute>) {
+    return this.http.put<IssueCustomAttribute>(`${this.base}/${id}`, us);
+  }
+
+  public patch(id: number, us: Partial<IssueCustomAttribute>) {
+    return this.http.patch<IssueCustomAttribute>(`${this.base}/${id}`, us);
+  }
+
+  public delete(id: number) {
+    return this.http.delete(`${this.base}/${id}`);
+  }
+
+  public bulkUpdateOrder(projectId: number, bulkIssueCustomAttributes: [number, number][]) {
+    return this.http.post(`${this.base}/bulk_update_order`, {
+      projectId,
+      bulkIssueCustomAttributes,
+    });
+  }
+
+}
---
+++ src/app/api/issues/issue-custom-attribute.model.mock.ts
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2014-2020 Taiga Agile LLC
+ *
+ * This source code is licensed under the terms of the
+ * GNU Affero General Public License found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import * as Factory from 'factory.ts';
+import { IssueCustomAttribute } from './issue-custom-attribute.model';
+import * as faker from 'faker';
+
+export const IssueCustomAttributeCreationDataMockFactory = Factory.Sync.makeFactory<IssueCustomAttribute>({
+  createdDate: faker.date.recent().toString(),
+  description: faker.lorem.paragraph(),
+  extra: faker.lorem.paragraph(),
+  id: faker.random.number(),
+  modifiedDate: faker.date.recent().toString(),
+  name: faker.random.word(),
+  order: faker.random.number(),
+  project: faker.random.number(),
+  type: faker.random.word(),
+});
