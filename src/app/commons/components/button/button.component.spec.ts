3ed651b48f9e6d6f861bf39bfb5c58d6c9eaa42c
Xaviju <xaviju@gmail.com>
2020-07-28 13:06:46 +0200

    feat(button): create example button component

--- src/app/commons/components/button/button.component.spec.ts
+++ src/app/commons/components/button/button.component.spec.ts
@@ -8,21 +8,21 @@
 
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ButtonComponent } from './button.component';
+import { TgButtonComponent } from './button.component';
 
 describe('ButtonComponent', () => {
-  let component: ButtonComponent;
-  let fixture: ComponentFixture<ButtonComponent>;
+  let component: TgButtonComponent;
+  let fixture: ComponentFixture<TgButtonComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ButtonComponent ],
+      declarations: [ TgButtonComponent ],
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ButtonComponent);
+    fixture = TestBed.createComponent(TgButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
