c3d6869a956feb5148649ebbc58c49b3ec01ae44
Juanfran <juanfran.ag@gmail.com>
2020-09-02 08:46:37 +0200

    fix(project-nav): fix admin url

--- src/app/commons/components/svg-sprite/svg-sprite.component.ts
+++ src/app/commons/components/svg-sprite/svg-sprite.component.ts
@@ -11,6 +11,9 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'tg-svg-sprite',
   templateUrl: './svg-sprite.component.html',
+  styles: [
+    `:host { display: none; }`,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TgSvgSpriteComponent {}
