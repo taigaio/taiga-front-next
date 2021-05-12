10c9b667b9175191bf1d1b7d9fdc59cab257c97f
Xaviju <xaviju@gmail.com>
2021-05-12 12:48:56 +0200

    fix: remove structural requirement for checklists

--- src/app/commons/text-editor/turndown-plugins/task-list-items.ts
+++ src/app/commons/text-editor/turndown-plugins/task-list-items.ts
@@ -11,9 +11,7 @@ import TurndownService from 'turndown';
 export default (turndownService: TurndownService) => {
   turndownService.addRule('taskListItems', {
     filter: (node: HTMLInputElement) => {
-      return node.type === 'checkbox' &&
-        !!(node.closest('label')) &&
-        !!(node.closest('li')) ;
+      return node.type === 'checkbox';
     },
     replacement: (_content, node: HTMLInputElement) => {
       return (node.checked ? '[x]' : '[ ]') + ' ';
